095e03c96384fc54dd069ac3c349a98d125d4a28
Clayton da Silva Rodrigues <clayton.metal2@hotmail.com>
2023-02-15 21:20:17 -0300

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import cors from 'cors';
+
+const routes = require("./routes");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(routes);
+
+app.listen(4000, () => console.log("Servidor rodando na porta 4000"));
